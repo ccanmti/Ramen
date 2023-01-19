b64d1847b223b88080f655eccf400927aa68e778
ccanmti <ccanmti@gmail.com>
2023-01-19 11:55:05 +0000

    add responsibe top

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,7 +1,7 @@
 class Public::HomesController < ApplicationController
   # トップページ
   def top
-    @shops = Shop.all.page(params[:page]).per(3).order('created_at DESC')   #店舗の表示数を３店舗のみとしている
+    @shops = Shop.all.page(params[:page]).per(4).order('created_at DESC')   #店舗の表示数を３店舗のみとしている
   end
 
 end
