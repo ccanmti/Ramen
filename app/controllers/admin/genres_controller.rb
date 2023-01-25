7dc0648c5f02e025de9a621b6ddd017e5206d0de
ccanmti <ccanmti@gmail.com>
2023-01-25 09:30:47 +0000

    edit

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -31,7 +31,7 @@ class Admin::GenresController < ApplicationController
     redirect_to admin_genres_path
   end
 
-  # ジャンルのストロングパラメータの設定s
+  # ジャンルのストロングパラメータの設定
   private
   def genre_params
     params.require(:genre).permit(:name)
